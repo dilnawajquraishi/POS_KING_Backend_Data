9f705066a9dcea14ebf57b607c1af53cb69d5b1c
dilnawajquraishi <dilnawajquraishi@gmil.com>
2024-08-31 12:35:53 +0530

    change

--- routes/ProductReport.js
+++ routes/ProductReport.js
@@ -3,7 +3,6 @@ const router = express.Router();
 // const productController = require('../controller/productController');
 const { getProductInventory } = require('../controller/ProductReport');
 
-// router.get('/product/inventory', productController.getProductInventory);
 router.get('/product/inventory',getProductInventory);
 
 
