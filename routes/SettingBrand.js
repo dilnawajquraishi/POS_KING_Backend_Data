cecf09ed4dc152e28bcdac146abdf600bf3b71cd
dilnawajquraishi <dilnawajquraishi@gmil.com>
2024-09-27 18:09:31 +0530

    Brand Route Update

--- routes/SettingBrand.js
+++ routes/SettingBrand.js
@@ -3,18 +3,20 @@ const express = require('express');
 const { settingBrandCreate, GetSettingBrand, SettingDeleteBrand, settingUpdateBrand, viewSettingBrand } = require('../controller/SettingBrand');
 const router = express.Router();
 
-
 // ----------------Setting_Brand-------------------
 
-router.post('/addBrand', settingBrandCreate)
+router.post('/addBrand', uploads.single('image'),settingBrandCreate)
+
+
+// router.put('/updateProduct/:id', , productController.updateProduct)
 
 router.get('/getAllBrand', GetSettingBrand)
 
 router.get('/viewBrand/:id', viewSettingBrand)
 
-router.get('/updateBrand/:_id', settingUpdateBrand)
+router.put('/updateBrand/:_id',uploads.single('image'), settingUpdateBrand)
 
-router.get('/deleteBrand/:_id', SettingDeleteBrand)
+router.delete('/deleteBrand/:_id', SettingDeleteBrand)
 
 
 module.exports = router
